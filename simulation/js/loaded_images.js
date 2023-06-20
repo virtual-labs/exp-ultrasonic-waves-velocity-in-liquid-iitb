97b685f9aa2ef26a96cd8bfa42e3b86d7dfb5dbf
vleadadmin <admin@vlabs.ac.in>
2023-06-20 12:27:13 +0000

    https://virtual-labs.github.io/virtual-labs/exp-ultrasonic-waves-velocity-in-liquid-iitb click on the link to test your code.

---
+++ simulation/js/loaded_images.js
@@ -0,0 +1,16 @@
+// 7 individual components for acitvity 1
+var ballast_tank = new Image();
+ballast_tank.src = './images/components/Ballast_Tank.webp';
+var condenser = new Image();
+condenser.src = './images/components/Condenser.webp';
+var n2_tank = new Image();
+n2_tank.src = './images/components/N2_Tank.webp';
+var dimmer_stat = new Image();
+dimmer_stat.src = './images/components/Temperature_Indicator2.webp';
+var connector = new Image();
+connector.src = './images/components/Connector.webp';
+var temp_indicator = new Image();
+temp_indicator.src = './images/components/Temperature_Indicator.webp';
+var ebulliometer = new Image();
+ebulliometer.src = './images/components/Ebulliometer.webp';
+//# sourceMappingURL=loaded_images.js.map
